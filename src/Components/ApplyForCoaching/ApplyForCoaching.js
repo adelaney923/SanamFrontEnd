bb2f0edf43033f201b4fb2b40210bb65a6f78380
“Alex <alexandriadelaney923@gmail.com>
2022-07-28 15:14:18 -0700

    updated/added questions on inquiry form

--- src/Components/ApplyForCoaching/ApplyForCoaching.js
+++ src/Components/ApplyForCoaching/ApplyForCoaching.js
@@ -34,15 +34,24 @@ const ApplyForCoaching = () => {
       <label>Email</label>
       <input type="email" name="user_email" />
       <label>Phone Number</label>
-      <input
-        type="tel"
-        name="user_phone"
-        pattern="[0-9]{10}"
-      />
-      <label>What is your biggest health goal?</label>
+      <input type="tel" name="user_phone" pattern="[0-9]{10}" />
+      <label>What is your BIGGEST health goal?</label>
       <textarea name="health_goal" />
-      <label>What has been your biggest obstacle in hitting that goal?</label>
+      <label>Why do you think you haven't hit the above goals yet?</label>
       <textarea name="obstacle" />
+      <label>
+        Are there any specific reasons why you would like to work with us?
+      </label>
+      <textarea name="why_us" />
+      <label>
+        On a scale of 1-10 (low to high), how comitted are you to achieving your
+        goals?
+      </label>
+      <input type="text" name="commitment_level" />
+      <label>
+        How did you hear about us?
+      </label>
+      <input type="text" name="hear_about_us" />
       <input type="submit" value="Send" />
     </form>
   );
